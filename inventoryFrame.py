73c6860878f07630bb221768c4a0e7bd4de61f45
Arjun Saili <arjun7.saili@gmail.com>
2023-04-06 01:47:33 -0400

    Create trees in inventory view

--- inventoryFrame.py
+++ inventoryFrame.py
@@ -1,12 +1,32 @@
 from tkinter import *
+from tkinter import ttk
+import sqlite3
 
 
 def make_inventory_frame(root, location):
+    inventory_frame = Frame(root)
+    inventory_frame.grid(row=0, column=0)
 
-    inventoryFrame = Frame(root, width=800, height=600)
-    inventoryFrame.grid(row=0, column=0)
-
-    test = Label(text=location)
-    test.place(anchor="center", relx=0.5, rely=0.5)
-
-    return inventoryFrame
+    try:
+        db = sqlite3.connect("python_db.db")
+        db_cursor = db.cursor()
+        rows = db_cursor.execute(
+            f"SELECT * FROM Inventory WHERE Store='{location}'")
+        inventory_tree = ttk.Treeview(inventory_frame, show="headings")
+        inventory_tree['columns'] = ("ID", "Item Name", "Count")
+        inventory_tree.column("ID", anchor=CENTER, stretch=NO, width=100)
+        inventory_tree.column("Item Name", anchor=CENTER, stretch=NO, width=300)
+        inventory_tree.column("Count", anchor=CENTER, stretch=NO, width=200)
+        inventory_tree.heading("ID", text="ID", anchor=W)
+        inventory_tree.heading("Item Name", text="Item", anchor=CENTER)
+        inventory_tree.heading("Count", text="Count ", anchor=W)
+        item_count = 0
+        for row in rows:
+            item_count += 1
+            inventory_tree.insert(parent='', index="end", iid=row[0],
+                                  values=(row[0], row[1], row[3]))
+        inventory_tree["height"] = item_count
+        inventory_tree.pack()
+    except sqlite3.Error as e:
+        print(f"Error {e.args[0]}")
+    return inventory_frame
